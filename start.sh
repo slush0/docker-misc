2538f12a645a26c65e6b26f84ca0c484944f14e7
slush0 <marek@palatinus.cz>
2023-03-01 00:10:42 +0000

    Updated tooling to make it actually work.

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker run -t --cidfile=bitcoind.cid \
+docker run --rm --cidfile=bitcoind.cid \
 	-v $(pwd)/data:/.bitcoin \
         -p 8333:8333 \
         -p 8332:8332 \
-	-d bitcoind $*
+	-d bitcoin $*
