f835d9c8722b4dd21c8c298dc9e4e33cce60b9e3
slush0 <marek@palatinus.cz>
2025-06-29 00:03:29 +0100

    bitcoin: Build system updated to bitcoin 29.0

--- bitcoin/start.sh
+++ bitcoin/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker run --rm --cidfile=bitcoind.cid \
-	-v $(pwd)/data:/.bitcoin \
+	-v $(pwd)/data:/data \
         -p 8333:8333 \
         -p 8332:8332 \
 	-d bitcoin $*
--- bitcoin/stop.sh
+++ bitcoin/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker stop -t 1 `cat bitcoind.cid`
-rm -f bitcoin.cid
+rm -f bitcoind.cid
